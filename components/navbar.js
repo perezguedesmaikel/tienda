24414f489a842cb804c7352524c12cd817fb74be
Maikel33 <perezguedesmaikel@gmail.com>
2022-08-01 14:39:27 -0400

    regular 2

--- components/navbar.js
+++ components/navbar.js
@@ -64,10 +64,7 @@ export default function NavBar(props) {
                 <Box sx={{ my: 2 }}>
                     {[...new Array(12)]
                         .map(
-                            () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+                            () => {props.children},
                         )
                         .join('\n')}
                 </Box>
