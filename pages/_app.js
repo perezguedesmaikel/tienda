5cd94ded15a9bef8f34bac0e9a329dfc7e2f83cd
Maikel33 <perezguedesmaikel@gmail.com>
2022-08-01 22:22:34 -0400

    no me rindo

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
 
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
